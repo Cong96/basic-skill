b130ca75ef25270444e404f504b55ea11b02673e
wangcongchong <wangcc@chaosource.com>
2020-01-04 21:57:17 +0800

    贪心算法

--- algorithm/src/main/java/com.leetcode/slidingwindow/MinWindowSolution.java
+++ algorithm/src/main/java/com.leetcode/slidingwindow/MinWindowSolution.java
@@ -13,46 +13,46 @@ import java.util.Map;
 public class MinWindowSolution {
 
     public static void main(String[] args) {
-        String s="ADOBECODEBANC";
-        String t="ABC";
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
     }
 
-    public String minWindow(String str,String target){
-        Map<Character,Integer>  windows=new HashMap<>();
-        Map<Character,Integer>  needs=new HashMap<>();
+    public String minWindow(String str, String target) {
+        Map<Character, Integer> windows = new HashMap<>();
+        Map<Character, Integer> needs = new HashMap<>();
         //将需要匹配的内容存入hash表
-        for(char c:target.toCharArray()){
-            needs.put(c,needs.getOrDefault(c,0)+1);
+        for (char c : target.toCharArray()) {
+            needs.put(c, needs.getOrDefault(c, 0) + 1);
         }
-        int left=0;
-        int right=0;
-        int match=0;
-        int start=0;
-        int min=Integer.MIN_VALUE;
+        int left = 0;
+        int right = 0;
+        int match = 0;
+        int start = 0;
+        int min = Integer.MIN_VALUE;
         //慢慢缩小滑动窗口
-        while(right<str.length()){
-            char rightChar=str.charAt(right);
-            if(needs.containsKey(rightChar)){
-                windows.put(rightChar,windows.getOrDefault(rightChar,0)+1);
-                if(windows.get(rightChar)==needs.get(rightChar)){
+        while (right < str.length()) {
+            char rightChar = str.charAt(right);
+            if (needs.containsKey(rightChar)) {
+                windows.put(rightChar, windows.getOrDefault(rightChar, 0) + 1);
+                if (windows.get(rightChar).equals(needs.get(rightChar))) {
                     match++;
                 }
             }
             //先达到目标条件，然后再缩小滑动窗口
-            while(match==needs.size()){
-                if(right-left+1<min){
-                    min=right-left+1;
-                    start=right;
+            while (match == needs.size()) {
+                if (right - left + 1 < min) {
+                    min = right - left + 1;
+                    start = right;
                 }
-                char leftChar=str.charAt(left);
-                if(needs.containsKey(leftChar)){
-                    windows.put(leftChar,windows.get(leftChar)-1);
-                    if(windows.get(leftChar)<needs.get(leftChar)){
+                char leftChar = str.charAt(left);
+                if (needs.containsKey(leftChar)) {
+                    windows.put(leftChar, windows.get(leftChar) - 1);
+                    if (windows.get(leftChar) < needs.get(leftChar)) {
                         match--;
                     }
                 }
             }
         }
-        return min==Integer.MAX_VALUE?"":str.substring(start,start+min);
+        return min == Integer.MAX_VALUE ? "" : str.substring(start, start + min);
     }
 }
---
+++ algorithm/src/main/java/com/ali/prepare/slidingwindow/LongsetSubStringSolution.java
@@ -0,0 +1,14 @@
+package com.ali.prepare.slidingwindow;
+
+/**
+ * @Author: BryantCong
+ * @Date: 2020/1/4 21:45
+ * @Description:
+ */
+public class LongsetSubStringSolution {
+
+
+    public String longestSubString(String str) {
+        return null;
+    }
+}
